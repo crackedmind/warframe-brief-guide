04324571a0e316b3f51c7693c9417aec204bbdff
Alexandr <68318783+Jorj-o-jorj@users.noreply.github.com>
2020-08-15 17:01:54 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Alexander Tomlenov'
 author = 'Alexander Tomlenov'
 
 # The full version, including alpha/beta/rc tags
-release = '28.1.1'
+release = '28.3.2'
 
 
 # -- General configuration ---------------------------------------------------
