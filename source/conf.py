19bc83f624142781fb9671de67c35be29f4eb41b
Jorj-o-jorj <atomlenov@yandex.ru>
2020-10-19 22:36:16 +0300

    правки

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Alexander Tomlenov'
 author = 'Alexander Tomlenov'
 
 # The full version, including alpha/beta/rc tags
-release = '29.2.4.5'
+release = '29.2.4.6'
 
 # -- General configuration ---------------------------------------------------
 
