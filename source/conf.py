4b7ff9371f1c4393c42e42fb3f71827f39e8fd25
Jorj-o-jorj <atomlenov@yandex.ru>
2021-05-25 11:17:47 +0300

    update02

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Alexander Tomlenov'
 author = 'Alexander Tomlenov'
 
 # The full version, including alpha/beta/rc tags
-release = '30.9.0'
+release = '30.9.1'
 
 # -- General configuration ---------------------------------------------------
 
